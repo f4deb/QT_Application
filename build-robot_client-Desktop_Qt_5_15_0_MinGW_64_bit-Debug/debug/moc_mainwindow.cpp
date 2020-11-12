41a10b60a050ca88193f6a172ce5e64e58bdde26
f4deb <f4deb1@gmail.com>
2020-11-12 21:58:20 +0100

    MAJ compile OK

---
+++ build-robot_client-Desktop_Qt_5_15_0_MinGW_64_bit-Debug/debug/moc_mainwindow.cpp
@@ -0,0 +1,164 @@
+/****************************************************************************
+** Meta object code from reading C++ file 'mainwindow.h'
+**
+** Created by: The Qt Meta Object Compiler version 67 (Qt 5.15.0)
+**
+** WARNING! All changes made in this file will be lost!
+*****************************************************************************/
+
+#include <memory>
+#include "../../robot_client/mainwindow.h"
+#include <QtCore/qbytearray.h>
+#include <QtCore/qmetatype.h>
+#if !defined(Q_MOC_OUTPUT_REVISION)
+#error "The header file 'mainwindow.h' doesn't include <QObject>."
+#elif Q_MOC_OUTPUT_REVISION != 67
+#error "This file was generated using the moc from 5.15.0. It"
+#error "cannot be used with the include files from this version of Qt."
+#error "(The moc has changed too much.)"
+#endif
+
+QT_BEGIN_MOC_NAMESPACE
+QT_WARNING_PUSH
+QT_WARNING_DISABLE_DEPRECATED
+struct qt_meta_stringdata_MainWindow_t {
+    QByteArrayData data[13];
+    char stringdata0[297];
+};
+#define QT_MOC_LITERAL(idx, ofs, len) \
+    Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
+    qptrdiff(offsetof(qt_meta_stringdata_MainWindow_t, stringdata0) + ofs \
+        - idx * sizeof(QByteArrayData)) \
+    )
+static const qt_meta_stringdata_MainWindow_t qt_meta_stringdata_MainWindow = {
+    {
+QT_MOC_LITERAL(0, 0, 10), // "MainWindow"
+QT_MOC_LITERAL(1, 11, 25), // "on_pushButtonQuit_clicked"
+QT_MOC_LITERAL(2, 37, 0), // ""
+QT_MOC_LITERAL(3, 38, 24), // "on_buttonQuitter_clicked"
+QT_MOC_LITERAL(4, 63, 24), // "on_actionQuitter_changed"
+QT_MOC_LITERAL(5, 88, 24), // "on_actionQuitter_toggled"
+QT_MOC_LITERAL(6, 113, 4), // "arg1"
+QT_MOC_LITERAL(7, 118, 26), // "on_actionQuitter_triggered"
+QT_MOC_LITERAL(8, 145, 27), // "on_actionA_propos_triggered"
+QT_MOC_LITERAL(9, 173, 25), // "on_Serial_Connect_clicked"
+QT_MOC_LITERAL(10, 199, 37), // "on_Serial_Baudrate_currentTex..."
+QT_MOC_LITERAL(11, 237, 24), // "on_Serial_Port_activated"
+QT_MOC_LITERAL(12, 262, 34) // "on_Serial_Port_currentIndexCh..."
+
+    },
+    "MainWindow\0on_pushButtonQuit_clicked\0"
+    "\0on_buttonQuitter_clicked\0"
+    "on_actionQuitter_changed\0"
+    "on_actionQuitter_toggled\0arg1\0"
+    "on_actionQuitter_triggered\0"
+    "on_actionA_propos_triggered\0"
+    "on_Serial_Connect_clicked\0"
+    "on_Serial_Baudrate_currentTextChanged\0"
+    "on_Serial_Port_activated\0"
+    "on_Serial_Port_currentIndexChanged"
+};
+#undef QT_MOC_LITERAL
+
+static const uint qt_meta_data_MainWindow[] = {
+
+ // content:
+       8,       // revision
+       0,       // classname
+       0,    0, // classinfo
+      10,   14, // methods
+       0,    0, // properties
+       0,    0, // enums/sets
+       0,    0, // constructors
+       0,       // flags
+       0,       // signalCount
+
+ // slots: name, argc, parameters, tag, flags
+       1,    0,   64,    2, 0x08 /* Private */,
+       3,    0,   65,    2, 0x08 /* Private */,
+       4,    0,   66,    2, 0x08 /* Private */,
+       5,    1,   67,    2, 0x08 /* Private */,
+       7,    0,   70,    2, 0x08 /* Private */,
+       8,    0,   71,    2, 0x08 /* Private */,
+       9,    0,   72,    2, 0x08 /* Private */,
+      10,    1,   73,    2, 0x08 /* Private */,
+      11,    1,   76,    2, 0x08 /* Private */,
+      12,    1,   79,    2, 0x08 /* Private */,
+
+ // slots: parameters
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::Bool,    6,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void,
+    QMetaType::Void, QMetaType::QString,    6,
+    QMetaType::Void, QMetaType::QString,    6,
+    QMetaType::Void, QMetaType::QString,    6,
+
+       0        // eod
+};
+
+void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, void **_a)
+{
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        auto *_t = static_cast<MainWindow *>(_o);
+        Q_UNUSED(_t)
+        switch (_id) {
+        case 0: _t->on_pushButtonQuit_clicked(); break;
+        case 1: _t->on_buttonQuitter_clicked(); break;
+        case 2: _t->on_actionQuitter_changed(); break;
+        case 3: _t->on_actionQuitter_toggled((*reinterpret_cast< bool(*)>(_a[1]))); break;
+        case 4: _t->on_actionQuitter_triggered(); break;
+        case 5: _t->on_actionA_propos_triggered(); break;
+        case 6: _t->on_Serial_Connect_clicked(); break;
+        case 7: _t->on_Serial_Baudrate_currentTextChanged((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        case 8: _t->on_Serial_Port_activated((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        case 9: _t->on_Serial_Port_currentIndexChanged((*reinterpret_cast< const QString(*)>(_a[1]))); break;
+        default: ;
+        }
+    }
+}
+
+QT_INIT_METAOBJECT const QMetaObject MainWindow::staticMetaObject = { {
+    QMetaObject::SuperData::link<QMainWindow::staticMetaObject>(),
+    qt_meta_stringdata_MainWindow.data,
+    qt_meta_data_MainWindow,
+    qt_static_metacall,
+    nullptr,
+    nullptr
+} };
+
+
+const QMetaObject *MainWindow::metaObject() const
+{
+    return QObject::d_ptr->metaObject ? QObject::d_ptr->dynamicMetaObject() : &staticMetaObject;
+}
+
+void *MainWindow::qt_metacast(const char *_clname)
+{
+    if (!_clname) return nullptr;
+    if (!strcmp(_clname, qt_meta_stringdata_MainWindow.stringdata0))
+        return static_cast<void*>(this);
+    return QMainWindow::qt_metacast(_clname);
+}
+
+int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
+{
+    _id = QMainWindow::qt_metacall(_c, _id, _a);
+    if (_id < 0)
+        return _id;
+    if (_c == QMetaObject::InvokeMetaMethod) {
+        if (_id < 10)
+            qt_static_metacall(this, _c, _id, _a);
+        _id -= 10;
+    } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
+        if (_id < 10)
+            *reinterpret_cast<int*>(_a[0]) = -1;
+        _id -= 10;
+    }
+    return _id;
+}
+QT_WARNING_POP
+QT_END_MOC_NAMESPACE
